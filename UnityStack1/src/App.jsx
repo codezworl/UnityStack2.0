8219324490af922aa562009443a4e05267f64681
CodeBro <mwaqaszafar76@gmail.com>
2025-02-17 12:10:21 +0500

    app jsx

--- UnityStack1/src/App.jsx
+++ UnityStack1/src/App.jsx
@@ -13,7 +13,6 @@ import Chat from "./pages/Chat";
 import ProjectPage from "./pages/project";// comment
 import Getexperthelp from "./pages/Get-expert-help";
 import Questiontread from "./pages/Question Thread";
-// import AnswerEditor from "./pages/Awnsereditor";
 import Dashboard from "./pages/Admin Panel/Dashboard";
 import StudentDashboard from "./components/Studentdashboard";
 import SessionShedule from "./pages/sessionShedule";
@@ -29,6 +28,20 @@ import SetPassword from "./pages/ResetPassword";
 import ContactPage from "./pages/Contact Us";
 import Feedback from "./pages/Give feedback";
 import AboutUs from "./pages/Aboutus";
+import AskQuestionPage from "./pages/askquestion";
+import PreviewQuestion from "./components/previewpage";
+import HireDeveloper from './pages/HireDeveloper';
+import LiveSessionForm from "./components/Sessionform";
+import TransactionsPage from "./pages/Admin Panel/ShowTranssactions";
+import FindExpert from "./pages/FindExperPage";
+import Codeverification from "./pages/Codeverification";
+import Resetpassword from "./pages/ResetPassword";
+import StudentSidebar from "./pages/StudentSidebar";
+import ProtectedRoute from "./components/ProtectedRoute";
+import StudentProfile from "./pages/studentprofile";
+import DeveloperDashboard from "./pages/DeveloperDashboard";
+import CompanyDashboard from "./pages/CompanyDashboard";
+
 const App = () => {
   return (
     <Router>
@@ -47,8 +60,6 @@ const App = () => {
         <Route path="/Getexperthelp" element={<Getexperthelp/>}/>
         <Route path="/questionthread/:id" element={<Questiontread />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/answereditor/:id" element={<AnswerEditor />} /> */}
-        <Route path="/studentdashboard" element={<StudentDashboard />} />
         <Route path="/sessionShedule" element={<SessionShedule />} />
         <Route path="/SessionHistory" element={<SessionHistory />} />
         <Route path="/account" element={<Account />} />
@@ -57,15 +68,30 @@ const App = () => {
         <Route path="/companiesprofile" element={<CompanyProfile />} />
         <Route path="/organizationsighnup" element={<Organizationsighnup />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/companydashboard" element={<Companydashboard />} />
+        <Route path="/companydashboard" element={<CompanyDashboard />} />
         <Route path="/otpVerification" element={<OtpVerification />} />
         <Route path="/setpassword" element={<SetPassword />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/askquestion" element={<AskQuestionPage />} />
+        <Route path="/previewquestion" element={<PreviewQuestion />} />
+        <Route path="/hire-developer" element={<HireDeveloper />} />
+        <Route path="/livesessionform" element={<LiveSessionForm />} />
+        <Route path="/transactions" element={<TransactionsPage />} />
+        <Route path="/findexpert" element={<FindExpert />} />
+        <Route path="/codeVerification" element={<Codeverification />} />
+        <Route path="/resetPassword" element={<Resetpassword />} />
+        <Route path="/studentSidebar" element={<StudentSidebar />} />
+        <Route path="/studentprofile" element={<StudentProfile />} />
+        <Route path="/developerdashboard" element={<DeveloperDashboard />} />
+        <Route element={<ProtectedRoute allowedRoles={["student"]} />}>
+        <Route path="/studentdashboard" element={<StudentDashboard />} />
+        </Route>
+        <Route path="*" element={<Home />} />
       </Routes>
-    </Router>// routes
+    </Router>
   );
 };
 
-export default App;
+export default App;
